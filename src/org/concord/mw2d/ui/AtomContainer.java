84ecdc138ecb4a76c95284520678e361380ad2f8
qxie <qxie@6e01202a-0783-4428-890a-84243c50cc2b>
2012-08-01 19:50:50 +0000

    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/simulations/mw@26129 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/mw2d/ui/AtomContainer.java
+++ src/org/concord/mw2d/ui/AtomContainer.java
@@ -300,11 +300,6 @@ public class AtomContainer extends MDContainer implements RNATranscriptionListen
 				String s = script.substring(matcher.end()).trim().toLowerCase();
 				if (s.startsWith("dna")) {
 					final String s2 = s.substring(3).trim();
-					int remainder = s2.length() % 3;
-					if (remainder != 0) {
-						JOptionPane.showMessageDialog(view, "Input DNA code must be 3xn! Please add " + (3 - remainder) + " or remove " + remainder + " nucleotide(s).", "Triplets required", JOptionPane.ERROR_MESSAGE, null);
-						return null;
-					}
 					char c;
 					for (int i = 0; i < s2.length(); i++) {
 						c = s2.charAt(i);
@@ -1158,14 +1153,9 @@ public class AtomContainer extends MDContainer implements RNATranscriptionListen
 
 		ActionListener okListener = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				final String s2 = dnaField.getText().trim();
-				int remainder = s2.length() % 3;
-				//if (remainder != 0) {
-					//JOptionPane.showMessageDialog(view, "Input DNA code must be 3xn! Please add " + (3 - remainder) + " or remove " + remainder + " nucleotide(s).", "Triplets required", JOptionPane.ERROR_MESSAGE, null);
-					//return;
-				//}
 				dnaPlayButton.setEnabled(true);
 				dnaScroller.reset();
+				final String s2 = dnaField.getText().trim();
 				EventQueue.invokeLater(new Runnable() {
 					public void run() {
 						dnaScroller.setDNA(new DNA(s2, contextDNA));
